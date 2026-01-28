153f904e3db72c0f8a8f273db3f481f2c288992f
vishnu <vishnuvardhankoppala123@gmail.com>
2026-01-28 08:08:47 +0530

    configrations

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const head=React.createElement("h1",{},"Hello World from ReactJS");
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(head);
---
+++ dist/reactJS.7c0ccee6.js
@@ -0,0 +1,6 @@
+im;
+const head = React.createElement("h1", {}, "Hello World from ReactJS");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(head);
+
+//# sourceMappingURL=reactJS.7c0ccee6.js.map
